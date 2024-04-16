de4e23db33eaee4e332f4dda115f02869218d2f0
Isaac Cohen <idcohen2000@gmail.com>
2024-04-16 00:04:03 -0400

    Added a switch example

---
+++ Basic syntax/switch_example.dart
@@ -0,0 +1,21 @@
+void main(){
+
+  int val = 10;
+
+  switch(val) {
+    case 0:
+      print("It's 0!");
+      break;
+    case 1:
+      print("It's 1!");
+      break;
+    case 2:
+      print("It's 2!");
+      break;
+    case 10:
+      print("It's 10!");
+      break;
+    default:
+      print("Something else!");
+  }
+}
